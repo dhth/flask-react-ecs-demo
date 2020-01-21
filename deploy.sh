73a3230a6a75745b16384a61942c351956742eae
Dhruv Thakur <dhruvt93@gmail.com>
2020-01-21 13:45:04 +0100

    added service update to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,21 +18,35 @@ register_definition() {
   fi
 }
 
+# new
+update_service() {
+  if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+    echo "Error updating service."
+    return 1
+  fi
+}
+
 deploy_cluster() {
 
+  cluster="experimental-flask-react-cluster" # new
+
   # users
+  service="experimental-flask-react-users-service"  # new
   template="ecs_users_taskdefinition.json"
   task_template=$(cat "ecs/$template")
   task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_URI $PRODUCTION_SECRET_KEY)
   echo "$task_def"
   register_definition
+  update_service  # new
 
   # client
+  service="experimental-flask-react-client-service"  # new
   template="ecs_client_taskdefinition.json"
   task_template=$(cat "ecs/$template")
   task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
   echo "$task_def"
   register_definition
+  update_service  # new
 
 }
 
